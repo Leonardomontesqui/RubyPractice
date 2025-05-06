fe9cd5c2c1bc9c22f6c43f0a0c4634555824f2d3
Leonardo Montes <leonardo@Leonardos-MacBook-Pro.local>
2025-05-06 17:29:29 -0400

    first

---
+++ hello.rb
@@ -0,0 +1,21 @@
+# This is a comment
+
+# Variable
+name = "Alice"
+
+# Method definition
+def greet(person)
+  puts "Hello, #{person}!"
+end
+
+# Class definition
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+
+# Code execution
+greet(name)
+dog = Dog.new
+dog.bark
